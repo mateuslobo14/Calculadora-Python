b0432b6fe083701c74875e52c6d1d6f74d1e5806
Mateus Lobo <114262469+mateuslobo14@users.noreply.github.com>
2022-11-17 01:06:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+var1 = float (input("Digite o primeiro número") )
+var2 = float (input("Digite o segundo número") )
+soma = var1 + var2 
+subtracao = var1 - var2
+mult = var1 * var2
+div = var1 / var2
+expo = var1 ** var2
+resto = var1 % var2
+print ("Soma:          ", var1, '+', var2, '=' ,  soma)
+print ('Subtração:     ', var1, '-', var2, '=' ,  subtracao)
+print ('Multiplicação  ', var1, '*', var2, '=' , mult)
+print ('Divisão        ', var1, '/', var2, '=' , div)
+print ('Exponenciação'  , var1, '**', var2, '=' , expo)
+print ('Resto da divisão', var1, '%', var2, '=' , resto)
+
